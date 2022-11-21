55f7513c1f6cb1633aa912dceb2fd6bda2e16b53
Kilvny <108346469+Kilvny@users.noreply.github.com>
2022-11-21 14:07:51 +0200

    Created Firestore database
    Connection between database and posts is managed as will as improvement to the feed

--- src/components/Body/Feed/CreateIcon.jsx
+++ src/components/Body/Feed/CreateIcon.jsx
@@ -4,12 +4,10 @@ import '../../../assets/createicon.css'
 
 const CreateIcon = ({Icon, title, color}) => {
   return (
-    <div className="icons">
         <div className='create-icon'>
             <Icon style={{ color: color}} />
             <h4>{title}</h4>
         </div>
-    </div>
   )
 }
 
---
+++ src/components/Body/Feed/Post.jsx
@@ -0,0 +1,42 @@
+import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
+import { Avatar } from '@mui/material'
+import React from 'react'
+import '../../../assets/post.css'
+import CreateIcon from './CreateIcon'
+import ChatOutlinedIcon from '@mui/icons-material/ChatOutlined';
+import RepeatOutlinedIcon from '@mui/icons-material/RepeatOutlined';
+import SendOutlinedIcon from '@mui/icons-material/SendOutlined';
+
+const Post = ({ name, job, message, avatarUrl}) => {
+  return (
+    <div className='post'>
+        <div className="post-header">
+            <Avatar/>
+            <div className="header-info">
+                <h3>{name && name.toUpperCase()}</h3> <span>• Following</span>
+                <p>{job}</p>
+            </div>
+        </div>
+        <div className="post-body">
+            <p>{message}</p>
+            {/* <p>The best way to learn to code is to build stuff. ⚙️🛠🧰🪛🪚 With the right strategy, you can even do it for free! 🤑 Take a look at how I created an online personal finance app with nothing out of pocket. 😎</p> */}
+            <div className="post-actions">
+                <div className="post-reaction">
+                <CreateIcon Icon={ThumbUpOffAltIcon} color='gray' title='Like'/>
+                </div>
+                <div className="post-reaction">
+                <CreateIcon Icon={ChatOutlinedIcon} color='gray' title='Comment'/>
+                </div>
+                <div className="post-reaction">
+                <CreateIcon Icon={RepeatOutlinedIcon} color='gray'title='Repost'/>
+                </div>
+                <div className="post-reaction-share">
+                <CreateIcon Icon={SendOutlinedIcon} color='gray' title='Send'/>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Post
